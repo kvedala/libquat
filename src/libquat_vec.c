73effd9e869afdf90f120111a775eaedb0f8cdb6
Krishna Vedala <kvedala@carepredict.com>
2015-10-01 06:29:32 -0400

    fixes + more API + mac tested

--- src/libquat_vec.c
+++ src/libquat_vec.c
@@ -1,10 +1,61 @@
+/** 
+ * @file libquat_vec.c
+ * API Functions related to 3D vector operations.
+ * @author Krishna Vedala
+ */
+
+
+#include <stdio.h>
+#ifdef __arm__
+#define LIBQUAT_ARM
+#include <arm_math.h>
+#else
 #include <math.h>
+#endif
 #include "libquat.h"
 
-float dot_prod(3d_vec *a, 3d_vec *b)
+void dot_prod(vec_3d *a, vec_3d *b, float *dot)
 {
-  float result = a->x * b->x;
-  result += a->y * b->y;
-  result += a->z * b->z;
-  return result;
+#ifdef LIBQUAT_ARM
+  arm_dot_prod_f32((float*) a, (float*) b, dot);
+#else
+  *dot = a->x * b->x;
+  *dot += a->y * b->y;
+  *dot += a->z * b->z;
+#endif
+}
+
+
+void vector_prod (vec_3d *a, vec_3d *b, vec_3d *o)
+{
+  o->x =  a->y * b->z - a->z * b->y;
+  o->y = -a->x * b->z + a->z * b->x;
+  o->z =  a->x * b->y - a->y * b->x;
+}
+
+
+void print_vector(vec_3d *a, const char *name)
+{
+  printf("vec(%s) = (%.3g)i + (%.3g)j + (%.3g)k\n", 
+      name, a->x, a->y, a->z);
+}
+
+
+void vector_norm(vec_3d *a, float *n)
+{
+  dot_prod(a, a, n);
+#ifdef LIBQUAT_ARM
+  arm_sqrt_f32(*n, n);
+#else
+  *n = sqrtf(*n);
+#endif
+}
+
+void unit_vec(vec_3d *a, vec_3d *n)
+{
+  float norm;
+  vector_norm(a, &norm);
+  n->x = a->x / norm;
+  n->y = a->y / norm;
+  n->z = a->z / norm;
 }
--- tests/test.c
+++ tests/test.c
@@ -10,9 +10,25 @@
 
 int main(void)
 {
-//  3d_vec a = {1.,2.,3.};
-//  3d_vec b = {1.,1.,1.};
+  vec_3d a = {1.,2.,3.};
+  vec_3d b = {1.,1.,1.};
+  vec_3d c;
+  float d;
 
-//  printf("Dot product: %f\n", dot_prod(&a, &b));
+  print_vector(&a, "a");
+  print_vector(&b, "b");
+
+  vector_norm(&a, &d);
+  printf("|a| = %.4g\n", d);
+  vector_norm(&b, &d);
+  printf("|b| = %.4g\n", d);
+
+  dot_prod(&a, &b, &d);
+  printf("Dot product: %f\n", d);
+
+  vector_prod(&a, &b, &c);
+  printf("Vector product ");
+  print_vector(&c, "c");
+  
   return 0;
 }
