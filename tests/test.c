146d0885febdc2eef7bc949137f33c1b55aefde6
Krishna Vedala <7001608+kvedala@users.noreply.github.com>
2020-08-20 15:40:19 -0400

    added assert statements to test.c

--- tests/test.c
+++ tests/test.c
@@ -7,6 +7,7 @@
 
 #include <stdio.h>
 #include <math.h>
+#include <assert.h>
 #include "../include/libquat.h"
 
 /**
@@ -34,6 +35,26 @@ int main(void) {
     c = vector_prod(&a, &b);
     printf("Vector product ");
     printf("%s", print_vector(&c, "c"));
+
+    quaternion quat = {0.7071f, 0.7071f, 0.f, 0.f};
+    euler eul = {0.f};
+    eul = euler_from_quat(&quat);
+    printf("Euler: % .3g, % .3g, % .3g\n", eul.pitch, eul.roll, eul.yaw);
+
+    quaternion test_quat;
+    test_quat = quat_from_euler(&eul);
+    printf("Quaternion: %+.3g %+.3g %+.3g %+.3g\n", test_quat.w,
+           test_quat.dual.x, test_quat.dual.y, test_quat.dual.z);
+
+    assert(fabsf(test_quat.q0 - quat.q0) < 0.1f);
+    assert(fabsf(test_quat.q1 - quat.q1) < 0.1f);
+    assert(fabsf(test_quat.q2 - quat.q2) < 0.1f);
+    assert(fabsf(test_quat.q3 - quat.q3) < 0.1f);
+
+    quaternion q1 = {1.f, -1.f, -1.f, 1.f};
+    quaternion q2 = {-1.f, -1.f, -1.f, -1.f};
+    quaternion q3 = quaternion_multiply(&q1, &q2);
+    printf("Quaternion: %+.3g %+.3g %+.3g %+.3g\n", q3.q0, q3.q1, q3.q2, q3.q3);
 
     return 0;
 }
